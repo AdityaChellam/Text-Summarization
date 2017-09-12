358fc7c76ca89ae63b0a9e8e098d659adb848dfa
AdityaChellam <AdityaChellam@users.noreply.github.com>
2017-09-12 23:35:33 +0530

    Normalized weights and sentences indexed
    Normalized weights and sentences indexed

--- textsum.py
+++ textsum.py
@@ -1,21 +1,31 @@
 import re
 import string
 
+"""to count the words and their frequencies"""
 frequency = {}
 file1=open("textpara.txt","r+");
 text_string = file1.read().lower()
-match_pattern = re.findall(r'\b[a-z]{4,25}\b', text_string)
+match_pattern = re.findall(r'\b[a-z]{3,25}\b', text_string)
  
 for word in match_pattern:
     count = frequency.get(word,0)
     frequency[word] = count + 1
      
 frequency_list = frequency.keys()
- 
+
+"""Normalized weights""" 
 for words in frequency_list:
-    print(words, frequency[words])
+    print(words, frequency[words]/sum(frequency.values()))
+file1.close()
 
-sentence = file1.read().lower()
+"""to index sentences"""
+print("\n\nPART 2")
+file1=open("textpara.txt","r+");
+sentence = file1.read().lower().split()
+for word in sentence:
+	if word[-1]==".":
+		print(word)
+file1.close()
 
-
+"""sentence-wise weight"""
 
